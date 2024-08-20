a0fa3d1c57881df79a9c70d5edd8c5fbaac71cb0
bhargava <bhargavaa2655@gmail.com>
2024-08-20 21:55:47 +0530

    nkn blueprint hakidhini

--- binary_tree_using_oops.cpp
+++ binary_tree_using_oops.cpp
@@ -30,6 +30,31 @@ node *insert(node *root,node *newnode){
     }
     return root;
 }
+
+// Function to print pre-order elements
+node *preorder(node *root,int n){
+    
+}
+
+// Function to print in-order elements
+void inorder(node *root){
+    
+}
+
+// Function to print post-order elements
+void postorder(node *root){
+    
+}
+
+// Function to search a node into the Binary tree
+node *dele(node *root,int key){
+
+}
+
+// Function to delete a node from the Binary tree
+node *dele(node *root,int n){
+
+}
 int main() {
     int ch;
     while(1){
@@ -41,8 +66,16 @@ int main() {
                     root=insert(root,newnode);  //Insert a newnode into the binary tree
                     break;
 
-            case 2 :dele();break;
-            case 3 :search();break;
+            case 2 :int n;
+                    cout<<"Enter the node : ";
+                    cin>>n;
+                    dele(root,n);break;
+
+            case 3 :int key;
+                    cout<<"Enter the node to be searched : ";
+                    cin>>key;
+                    //search(root,key);break;
+
             case 4 :preorder();break;
             case 5 :inorder();break;
             case 6 :postorder();break;
