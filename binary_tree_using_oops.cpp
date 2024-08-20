62c8ed3b34dc3b09069ed69e75b1897b3a55b691
bhargava <bhargavaa2655@gmail.com>
2024-08-20 21:43:57 +0530

    modified, insert and create in switch case

--- binary_tree_using_oops.cpp
+++ binary_tree_using_oops.cpp
@@ -5,24 +5,27 @@ class node{
     node *left;
     int data;
     node *right;
-}*root,*newnode;
+};
+node *root=NULL;
 
+// Function to create a new node
 node *create(){
-    newnode=new node();
-    cout<<"Enter fat data : ";
+    node *newnode=new node();
+    cout<<"Enter data : ";
     cin>>newnode->data;
     newnode->left=NULL;
     newnode->right=NULL;
     return newnode;
 }
+
+// Function to insert a node into the Binary tree
 node *insert(node *root,node *newnode){
     if(root==NULL){
-        root=create();
+        return newnode;
     }
     if(newnode->data < root->data){
         root->left=insert(root->left,newnode);
-    }
-    if(newnode->data > root->data){
+    }else if(newnode->data > root->data){
         root->right=insert(root->right,newnode);
     }
     return root;
@@ -30,17 +33,20 @@ node *insert(node *root,node *newnode){
 int main() {
     int ch;
     while(1){
-        cout<<"\n1.insert\n2.delete\n3.search\n4.preorder\n5.insert\n6.insert\n7.Exit\n";
+        cout<<"\n1.Insert\n2.Delete\n3.Search\n4.Preorder\n5.Inorder\n6.Post-order\n7.Exit\n";
         cout<<"Enter your choice : ";
         cin>>ch;
         switch(ch){
-            case 1 : root=insert();break;
-            case 2 : dele();break;
-            case 3 : search();break;
-            case 4 : preorder();break;
-            case 5 : inorder();break;
-            case 6 : postorder();break;
-            case 7 : exit(0);break;
+            case 1 :node *newnode=create();  //create a newnode 
+                    root=insert(root,newnode);  //Insert a newnode into the binary tree
+                    break;
+
+            case 2 :dele();break;
+            case 3 :search();break;
+            case 4 :preorder();break;
+            case 5 :inorder();break;
+            case 6 :postorder();break;
+            case 7 :exit(0);
             default:cout<<"Invalid Choice"<<endl;
         }
     }
