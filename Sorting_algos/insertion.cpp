b7d82e436feacc2075b7cc52a1fc64ed1b22e1f0
bhargava <bhargavaa2655@gmail.com>
2024-08-27 23:06:38 +0530

    learnt insertion sort

--- Sorting_algos/insertion.cpp
+++ Sorting_algos/insertion.cpp
@@ -14,11 +14,10 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    for(int i=1;i<n;i++){
+    for( int i=1 ;i<n;i++){
         int temp=arr[i];
         int j=i-1;
-        while (j>=0&&arr[j]>temp)
-        {
+        while(j>=0&&arr[j]>temp){
             arr[j+1]=arr[j];
             j--;
         }
