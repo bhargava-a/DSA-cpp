03832948923b54092743ea327d9c175b444bbeb4
bhargava <bhargavaa2655@gmail.com>
2024-08-27 22:36:56 +0530

    insertion sort

--- Sorting_algos/insertion.cpp
+++ Sorting_algos/insertion.cpp
@@ -14,6 +14,19 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-
+    for(int i=1;i<n;i++){
+        int temp=arr[i];
+        int j=i-1;
+        while (j>=0&&arr[j]>temp)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+    cout<<"Sorted array : ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"\t";
+    }
     return 0;
 }
