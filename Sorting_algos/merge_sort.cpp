e83b18757f859f82204c0037cf84e9ff06c31860
bhargava <bhargavaa2655@gmail.com>
2024-08-27 23:40:42 +0530

    created a merge sort file.cpp

---
+++ Sorting_algos/merge_sort.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the array size : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array elements"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    return 0;
+}
