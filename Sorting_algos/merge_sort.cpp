755738f635c15209a4352652815579d3f9e827ed
bhargava <bhargavaa2655@gmail.com>
2024-08-28 22:35:14 +0530

    almost done

--- Sorting_algos/merge_sort.cpp
+++ Sorting_algos/merge_sort.cpp
@@ -1,5 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+void merge(int arr[],int lb,int mid,int ub){
+    int i=lb;
+    int j=mid+1;
+    int k=ub;
+    while(i<=mid&&j<=ub){
+        if(arr[i]<=arr[j]){
+            b[k]=arr[i];
+            i++;
+        }else{
+            b[k]=arr[j];
+            j++;
+        }
+        k++;
+    }
+    if(i>mid){
+        
+    }
+}
+void mergesort(int arr[],int lb,int ub){
+    if(lb<ub){
+        int mid=(lb+ub)/2;
+        mergesort(arr,lb,mid);
+        mergesort(arr,mid+1,ub);
+        merge(arr,lb,mid,ub);
+    }
+}
 
 int main(){
     int n;
