6b185a772aae9ad73b06431aa7dd132f09c93aea
BHARGAVA A <bhargavaa2655@gmail.com>
2024-08-29 22:22:05 +0530

    Update avl_tree.cpp

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std;
 class node{
-node *left,*right;
-int data;
-}*root;
+    node * left,*right;
+    int data;
+}*root=NULL;
 int main (){
-int ch;
-while(1){
-
-}
-return 0;
+    int choice;
+    while(1){
+        cout<<"\n1.insert\n2.delete"
+        "\n3.search\n4.pre-order"
+        "\n5.In-order\n6.post-order"
+        "\n7.Exit"<<endl;
+    }
 }
