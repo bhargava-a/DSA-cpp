2685d3ac1f62c15c9f195be8353d3752f9648ad0
BHARGAVA A <bhargavaa2655@gmail.com>
2024-09-02 22:47:40 +0530

    Update avl_tree.cpp

--- Non-Linear_data_structure/avl_tree.cpp
+++ Non-Linear_data_structure/avl_tree.cpp
@@ -42,6 +42,13 @@ node* leftRotate(node* x) {
     // Perform rotation
     y->left = x;
     x->right = T2;
+// Update heights
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    // Return new root
+    return y;
+}
 node *insert(node *root,node *newnode){
     if(root==NULL){
         return newnode;
