052388c65c2dfb796dc8bea9e92ebc40676304a0
bhargava <bhargavaa2655@gmail.com>
2024-10-02 22:34:29 +0530

    updated linked list with oops

--- Linear_data_structure/linked_list.cpp
+++ Linear_data_structure/linked_list.cpp
@@ -1,180 +1,258 @@
 #include<iostream>
 using namespace std;
-class node{
-    public:
-        int data;
-        node* next;
-}*head=NULL,*newnode,*temp;
 
+class node {
+public:
+    int data;
+    node* next;
+};
 
-void dis(){
-    temp=head;
-    while(temp!=NULL){
-        cout<<temp->data;
-        if(temp->next!=NULL){
-            cout<<"->"<<" ";
-        }
-        temp=temp->next;
-    }
-    cout<<"->NULL";
-}
-int len(){
-    int length=0;
-    temp=head;
-    while(temp!=NULL){
-        temp=temp->next;
-        length++;
-    }
-    return length;
-}
-void insert_at_begin(){
-    newnode=new node();
-    cout<<"Enter data : ";
-    cin>>newnode->data;
-    newnode->next=NULL;
-    if(head==NULL){
-        head=temp=newnode;
-    }else{
-        newnode->next=head;
-        head=newnode;
-    }
-    dis();
-}
-void insert_at_end(){
-    newnode=new node();
-    cout<<"Enter data : ";
-    cin>>newnode->data;
-    newnode->next=NULL;
-    if(head==NULL){
-        head=temp=newnode;
-    }else{
-        temp=head;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=newnode;
-    }
-    dis();
-}
-void insert_at_loc(){
-    int pos,i=1,length=len();
-    cout<<"Enter the position : ";
-    cin>>pos;
+class LL {
+private:
+    node* head, * temp, * newnode;
 
-    if(pos>length){
-       cout<<"invalid position"<<endl;
-    }else{
-        // TRAVERSING Part
-        temp=head;
-        while(i<pos-1){
-            temp=temp->next;
-            i++;
-        }
-        //linking part
-        newnode=new node();
-        cout<<"Enter data : ";
-        cin>>newnode->data;
-        newnode->next=temp->next;
-        temp->next=newnode;
+public:
+    // Constructor (now public)
+    LL() {
+        head = nullptr;
     }
-    dis();
-}
-void delete_at_begin(){
-    if(head==NULL){
-        cout<<"list is empty"<<endl;
-    }
-    cout<<"Deleted node is : "<<head->data<<endl;
-    temp=head;
-    head=head->next;
-    delete temp;
-    dis();
-}
-void delete_at_end(){
-    if(head==NULL){
-        cout<<"list is empty"<<endl;
-    }
-    node* dele;
-    temp=head;
-    while(temp->next!=NULL){
-        dele=temp;
-        temp=temp->next;
-    }
-    cout<<"Deleted node : "<<temp->data<<endl;
-    dele->next=NULL;
-    delete temp;
-    dis();
-}
-void delete_at_loc(){
-    int pos,i=1,length=len();
-    cout<<"Enter the pos : ";
-    cin>>pos;
-    if(pos>length){
-        cout<<"Invalid position"<<endl;
-    }else if(head==NULL){
-        cout<<"list is empty"<<endl;
-    }else{
-        temp=head;
-        while(i<pos-1){
-            temp=temp->next;
-            i++;
-        }
-        node* kill=temp->next;
-        temp->next=kill->next;
-        cout<<"Deleted node is : "<<kill->data<<endl;
-        delete kill;
-    }
-    dis();
-}
 
-void reverse(){
-    node* prev=NULL,*current,*nextnode;
-    current=nextnode=head;
-    while(nextnode!=NULL){
-        nextnode=nextnode->next;
-        current->next=prev;
-        prev=current;
-        current=nextnode;
+    // Display the linked list
+    void dis() {
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        temp = head;
+        while (temp != nullptr) {
+            cout << temp->data;
+            if (temp->next != nullptr) {
+                cout << "->";
+            }
+            temp = temp->next;
+        }
+        cout << "->NULL" << endl;
     }
-    head=prev;
-    dis();
-}
-int main(){
+
+    // Get the length of the list
+    int len() {
+        int length = 0;
+        temp = head;
+        while (temp != nullptr) {
+            temp = temp->next;
+            length++;
+        }
+        return length;
+    }
+
+    // Insert a node at the beginning
+    void insert_at_begin(int data) {
+        newnode = new node();
+        newnode->data = data;
+        newnode->next = head;
+        head = newnode;
+        dis();
+    }
+
+    // Insert a node at the end
+    void insert_at_end(int data) {
+        newnode = new node();
+        newnode->data = data;
+        newnode->next = nullptr;
+        if (head == nullptr) {
+            head = newnode;
+        }
+        else {
+            temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newnode;
+        }
+        dis();
+    }
+
+    // Insert a node at a specific location
+    void insert_at_loc(int pos, int data) {
+        int length = len();
+        if (pos > length + 1 || pos < 1) {
+            cout << "Invalid position" << endl;
+            return;
+        }
+
+        newnode = new node();
+        newnode->data = data;
+
+        if (pos == 1) {
+            newnode->next = head;
+            head = newnode;
+        }
+        else {
+            temp = head;
+            int i = 1;
+            while (i < pos - 1 && temp != nullptr) {
+                temp = temp->next;
+                i++;
+            }
+            newnode->next = temp->next;
+            temp->next = newnode;
+        }
+        dis();
+    }
+
+    // Delete the node at the beginning
+    void delete_at_begin() {
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        temp = head;
+        cout << "Deleted node is: " << head->data << endl;
+        head = head->next;
+        delete temp;
+        dis();
+    }
+
+    // Delete the node at the end
+    void delete_at_end() {
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        if (head->next == nullptr) {
+            cout << "Deleted node is: " << head->data << endl;
+            delete head;
+            head = nullptr;
+        }
+        else {
+            temp = head;
+            node* prev = nullptr;
+            while (temp->next != nullptr) {
+                prev = temp;
+                temp = temp->next;
+            }
+            cout << "Deleted node is: " << temp->data << endl;
+            prev->next = nullptr;
+            delete temp;
+        }
+        dis();
+    }
+
+    // Delete the node at a specific location
+    void delete_at_loc(int pos) {
+        int length = len();
+        if (pos > length || pos < 1) {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        if (head == nullptr) {
+            cout << "List is empty." << endl;
+            return;
+        }
+
+        if (pos == 1) {
+            delete_at_begin();
+        }
+        else {
+            temp = head;
+            int i = 1;
+            while (i < pos - 1) {
+                temp = temp->next;
+                i++;
+            }
+            node* kill = temp->next;
+            temp->next = kill->next;
+            cout << "Deleted node is: " << kill->data << endl;
+            delete kill;
+            dis();
+        }
+    }
+
+    // Reverse the linked list
+    void rev() {
+        node* prev = nullptr, * current = head, * nextnode;
+        while (current != nullptr) {
+            nextnode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextnode;
+        }
+        head = prev;
+        dis();
+    }
+
+    // Wrapper functions for public access
+    void insertAtbegin(int data) { insert_at_begin(data); }
+    void insertAtlast(int data) { insert_at_end(data); }
+    void insertAtloc(int pos, int data) { insert_at_loc(pos, data); }
+    void deleteAtbegin() { delete_at_begin(); }
+    void deleteAtlast() { delete_at_end(); }
+    void deleteAtloc(int pos) { delete_at_loc(pos); }
+    void reverse() { rev(); }
+    void display() { dis(); }
+};
+
+int main() {
     int choice;
-    while(1){
-        cout<<"\n\n1.insert at beginning\n2.insert at end\n3.insert at specific location"
-        "\n4.delete at beginning\n5.delete at end\n6.delete at specific location"
-        "\n7.display\n8.reverse\n9.Exit"<<endl;
-        cout<<"Enter your choice : ";
-        cin>>choice;
-        switch(choice){
-            case 1:insert_at_begin();break;
-            case 2:insert_at_end();break;
-            case 3:insert_at_loc();break;
-            case 4:delete_at_begin();break;
-            case 5:delete_at_end();break;
-            case 6:delete_at_loc();break;
-            case 7:dis();break;
-            case 8:reverse();break;
-            case 9:exit(0);break;
-            default:cout<<"Invalid choice"<<endl;
+    LL linked_list;
+
+    while (1) {
+        cout << "\n\n1. Insert at beginning\n2. Insert at end\n3. Insert at specific location"
+             << "\n4. Delete at beginning\n5. Delete at end\n6. Delete at specific location"
+             << "\n7. Display\n8. Reverse\n9. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                int x;
+                cout << "Enter a value: ";
+                cin >> x;
+                linked_list.insertAtbegin(x);
+                break;
+            }
+            case 2: {
+                int x;
+                cout << "Enter a value: ";
+                cin >> x;
+                linked_list.insertAtlast(x);
+                break;
+            }
+            case 3: {
+                int x, position;
+                cout << "Enter the position: ";
+                cin >> position;
+                cout << "Enter a value: ";
+                cin >> x;
+                linked_list.insertAtloc(position, x);
+                break;
+            }
+            case 4:
+                linked_list.deleteAtbegin();
+                break;
+            case 5:
+                linked_list.deleteAtlast();
+                break;
+            case 6: {
+                int loca;
+                cout << "Enter the location: ";
+                cin >> loca;
+                linked_list.deleteAtloc(loca);
+                break;
+            }
+            case 7:
+                linked_list.display();
+                break;
+            case 8:
+                linked_list.reverse();
+                break;
+            case 9:
+                exit(0);
+                break;
+            default:
+                cout << "Invalid choice" << endl;
         }
     }
     return 0;
 }
-/*void reverse() {
-    node* temp = head;
-    int n = len();
-    int* arr = new int[n]; // Dynamic array to store the values
-    int i = 0;
-    while (temp != NULL) {
-        arr[i] = temp->data;
-        temp = temp->next;
-        i++;
-    }
-    cout << "Reversed list: ";
-    for (int i = n - 1; i >= 0; i--) {
-        cout << arr[i] << "\t";
-    }
-    cout << endl;
-    delete[] arr; // Free the dynamically allocated memory
-}*/
