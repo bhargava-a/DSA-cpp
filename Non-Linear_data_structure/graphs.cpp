74df7baf41933584711dd3c0fe777c47b6820e74
bhargava <bhargavaa2655@gmail.com>
2024-10-02 21:13:11 +0530

    ;

--- Non-Linear_data_structure/graphs.cpp
+++ Non-Linear_data_structure/graphs.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int nodes,edges;
+    cout<<"Enter the number of nodes : ";
+    cin>>nodes;
+    cout<<"Enter the number of edges : ";
+    cin>>edges;
+    int matrix[nodes][nodes];
+    for(int i=0;i<nodes;i++){
+        for(int j=0;j<nodes;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    cout<<"Adjency MAtrix"<<endl;
+    for(int i=0;i<nodes;i++){
+        for(int j=0;j<nodes;j++){
+            cout<<matrix[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
     return 0;
 }
