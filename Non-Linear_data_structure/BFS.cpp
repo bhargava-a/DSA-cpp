41beac98b39943fceb7c04caec6855db46c82d38
bhargava <bhargavaa2655@gmail.com>
2024-10-05 22:02:39 +0530

    adjency matrix done

--- Non-Linear_data_structure/BFS.cpp
+++ Non-Linear_data_structure/BFS.cpp
@@ -6,11 +6,12 @@ int main(){
     cout<<"Enter the number of vertices : ";
     cin>>vertices;
     vector<vector<int>> adj(vertices,vector<int>(vertices,0));
+    cout<<"Enter the adjency matrix values :"<<endl;
     for(int i=0;i<vertices;i++){
         for(int j=0;j<vertices;j++){
-            cout<<adj[i][j]<<"\t";
+            cin>>adj[i][j];
         }
-        cout<<endl;
     }
+    
     return 0;
 }
