f6d67ffdd3db99249737ead02116ccc7f392fd44
bhargava <bhargavaa2655@gmail.com>
2024-10-05 22:17:58 +0530

    updated BFS file

--- Non-Linear_data_structure/BFS.cpp
+++ Non-Linear_data_structure/BFS.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
+void BFS(){
+    
+}
 int main(){
     int vertices;
     cout<<"Enter the number of vertices : ";
     cin>>vertices;
+    //created a adjacency matirx
     vector<vector<int>> adj(vertices,vector<int>(vertices,0));
     cout<<"Enter the adjency matrix values :"<<endl;
     for(int i=0;i<vertices;i++){
@@ -12,6 +17,16 @@ int main(){
             cin>>adj[i][j];
         }
     }
-    
+    //created a boolean queue for tracking the visited nodes
+    vector<bool> visited(vertices,false);
+
+    //calling for BFS for each component of the graph
+    for(int i=0;i<vertices;i++){
+        if(!visited[i]){
+            cout<<"BFS staring from vertex "<<i<<": ";
+            BFS(i,adj,visited);
+            cout<<endl;
+        }
+    }
     return 0;
 }
