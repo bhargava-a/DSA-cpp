a24232f654922dbc5266fa3ae5b7eb5ac3f3be59
bhargava <bhargavaa2655@gmail.com>
2024-10-05 23:34:18 +0530

    created dfs file

---
+++ Non-Linear_data_structure/DFS.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+int main(){
+    int vertices;
+    cout<<"Enter the no of vertices : ";
+    cin>>vertices;
+    vector<vector<int>> adj(vertices,vector<int> (vertices,0));
+    cout<<"Enter the adjacent matrix values : "<<endl;
+    for(int i=0;i<vertices;i++){
+        for(int j=0;j<vertices;j++){
+            cin>>adj[i][j];
+        }
+    }
+    for(int i=0;i<vertices;i++){
+        vector<bool> visited(vertices,false);
+        cout<<"DFS staring from vertex "<<i<<": ";
+        
+    }
+    return 0;
+}
