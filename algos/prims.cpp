3f73e8c9fe4cc734fb6a0bb0013d638252946529
bhargava <bhargavaa2655@gmail.com>
2024-10-10 21:30:13 +0530

    update

--- algos/prims.cpp
+++ algos/prims.cpp
@@ -5,13 +5,14 @@ int main(){
   int vertices;
   cout<<"Enter the no of Vertices : ";
   cin>>vertices;
-  cout<<"Enter the adjaceny matrix "<<endl;
-  vector<vector<int>> adj(vertices,vector<int>(vertices,0));
+  cout<<"Enter the cost matrix 0 for self loop and 999 for no path"<<endl;
+  vector<vector<int>> cost(vertices,vector<int>(vertices,0));
   for(int i=0;i<vertices;i++){
     for(int j=0;j<vertices;j++){
-      cin>>adj[i][j];
+      cin>>cost[i][j];
     }
   }
   
+
   return 0;
 }
