e16d6b068aaf7af2be547c24d5cc6317adf7f89f
bhargava <bhargavaa2655@gmail.com>
2024-10-17 14:37:30 +0530

    kruskal's algo

---
+++ algos/kruskal.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int main(){
+    int vertices;
+    cout<<"Enter the num of vertices : "<<flush;
+    cin>>vertices;
+    cout<<"Enter the cost matrix "<<endl;
+    vector<vector<int>> cost(vertices,vector<int>(vertices,0));
+    for(int i=0;i<vertices;i++){
+        for(int j=0;j<vertices;j++){
+            cin>>cost[i][j];
+        }
+    }
+    int source;
+    cout<<"Enter the source vector : "<<flush;
+    cin>>source;
+    
+    return 0;
+}
